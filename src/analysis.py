5f4fe77d006a3e801c0f716a66bc04bf883f8813
Luka Milic <lukam321@gmail.com>
2016-05-26 10:48:21 +0100

    +analysis.py: outline of a possible class for experiments

---
+++ src/analysis.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+class Analysis:
+    """
+    General analysis class which can either collect data during a run or take a
+    dictionary describing a previous run and offer its plotting and metric functions
+    """
+    def __init__(self,old_dictionary=None,tf_sess=None):
+        # Juist Initialize data
+        if old_dictionary == None:
+            pass
+        else:
+            pass
+
+    def add_cost(self,name,value):
+        pass
+
+    def get_metrics(self):
+        pass
+
+    def plot_autoencoder(self):
+        pass
+
+    def plot_costs(self):
+        pass
+
+    def archive(self):
+        pass
