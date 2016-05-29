285183d28e81902f18a67230b03ca0032f126b4b
Luka Milic <lukam321@gmail.com>
2016-05-29 15:36:23 +0100

    main.py now dumps lots of numerical data and plotexp.py plots it

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
-"""
-Lets try and load the DISFA data
-"""
-
 import numpy as np
-import matplotlib.pyplot as plt
 import h5py
+import metric
 import os
 from scipy import ndimage
 import scipy
-import seb.data_array as data_array
-import seb.disfa as disfa
+import socket
+from sklearn.metrics import roc_curve, auc
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from pyexp.pyexp import PyExp
+import math
+import tensorflow as tf
+import sys
 
 def prefix(i,zeros):
     s = str(i)
@@ -17,142 +20,197 @@ def prefix(i,zeros):
         s = '0' + s
     return s
 
-import math
-import tensorflow as tf
+def main(data,experiment):
 
-all_images = np.empty((0, 48, 48))
-#for s in [1]:
-for s in [1,2,3,4,5,6,7,8,9,10,11,12,13,16,17,18,21,23,24,25,26,27,28,29,30,31,32]:
-    images = disfa.disfa['images'][s][:]
-    resized_images = np.zeros((images.shape[0],48,48))
-    for i in xrange(images.shape[0]):
-        resized_images[i,:,:] = scipy.misc.imresize(images[i,:,:],[48,48]).astype(float)/float(255)
-    del images
-    all_images = np.append(all_images,resized_images,axis=0)
-    print all_images.shape
+    def weight_variable(shape):
+      # initial = tf.truncated_normal(shape, stddev=0.1)
+      # return tf.Variable(initial)
+      return tf.Variable(tf.random_uniform(shape, -1.0 / math.sqrt(shape[0]), 1.0 / math.sqrt(shape[0])))
 
-targets_all, id_array = data_array.IndicesCollection(disfa.disfa_ic_all).getitem(disfa.disfa['AUall'])
+    def bias_variable(shape):
+      # initial = tf.constant(0.1, shape=shape)
+      # return tf.Variable(initial)
+      return tf.Variable(tf.constant(0.1,shape=shape))
 
-targets_all = targets_all.astype(float)
-x,y = targets_all.shape
-for i in xrange(x):
-    for j in xrange(y):
-        if targets_all[i,j] > 0:
-            targets_all[i,j] = 1.0
-        else:
-            targets_all[i,j] = 0.0
-    sum = float(targets_all[i,:].sum())
-    if sum != 0.0:
-        targets_all[i,:] = targets_all[i,:]/sum
+    def conv2d(x, W,padding='SAME'):
+        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding=padding)
 
-def weight_variable(shape):
-  # initial = tf.truncated_normal(shape, stddev=0.1)
-  # return tf.Variable(initial)
-  return tf.Variable(tf.random_uniform(shape, -1.0 / math.sqrt(shape[0]), 1.0 / math.sqrt(shape[0])))
+    def max_pool_axb(x,a,b):
+        return tf.nn.max_pool(x, ksize=[1, a, b, 1],strides=[1, a, b, 1], padding='SAME')
+    size = experiment['image_size']
+    x = tf.placeholder(np.float32, shape=[None, size,size])
+    x_ = tf.reshape(x, [-1,size,size,1])
+    y_ = tf.placeholder(np.float32, shape=[None, 12])
 
-def bias_variable(shape):
-  # initial = tf.constant(0.1, shape=shape)
-  # return tf.Variable(initial)
-  return tf.Variable(tf.constant(0.5,shape=shape))
+    print 'Input: ', x_.get_shape()
+    print 'Outut: ', y_.get_shape()
+    convolution_network = False
+    if experiment['network'] == 'convolution_gudi_2015':
+        W_conv1 = weight_variable([5, 5, 1, 64])
+        b_conv1 = bias_variable([64])
 
-def conv2d(x, W,padding='SAME'):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding=padding)
+        print 'Convolution 1 Shape: ', W_conv1.get_shape(), ' with bias ', b_conv1.get_shape(), ' padding VALID'
 
-def max_pool_axb(x,a,b):
-    return tf.nn.max_pool(x, ksize=[1, a, b, 1],strides=[1, a, b, 1], padding='SAME')
+        h_conv1 = tf.nn.relu(conv2d(x_, W_conv1,padding='VALID') + b_conv1)
+        print ' output : ', h_conv1.get_shape()
+        h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 3, 3, 1],strides=[1, 2, 2, 1],padding='SAME')
+        print 'Max pool 3x3, stride 2x2, padding SAME'
+        print ' output : ', h_pool1.get_shape()
 
-x = tf.placeholder(np.float32, shape=[None, 48,48])
-x_ = tf.reshape(x, [-1,48,48,1])
-y_ = tf.placeholder(np.float32, shape=[None, 12])
+        W_conv2 = weight_variable([5, 5, 64, 64])
+        b_conv2 = bias_variable([64])
 
-W_conv1 = weight_variable([5, 5, 1, 64])
-b_conv1 = bias_variable([64])
+        print 'Convolution 2 Shape: ', W_conv2.get_shape(), ' with bias ', b_conv2.get_shape(), ' padding VALID'
 
-h_conv1 = tf.nn.relu(conv2d(x_, W_conv1,padding='VALID') + b_conv1)
-h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 3, 3, 1],strides=[1, 2, 2, 1],padding='SAME')
+        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2,padding="VALID") + b_conv2)
+        print ' output : ', h_conv2.get_shape()
 
-print h_conv1.get_shape()
-print h_pool1.get_shape()
+        W_conv3 = weight_variable([4, 4, 64, 128])
+        b_conv3 = bias_variable([128])
 
-W_conv2 = weight_variable([5, 5, 64, 64])
-b_conv2 = bias_variable([64])
+        print 'Convolution 3 Shape: ', W_conv3.get_shape(), ' with bias ', b_conv3.get_shape(), ' padding VALID'
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2,padding="VALID") + b_conv2)
+        h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3,padding="VALID") + b_conv3)
+        print ' output : ', h_conv3.get_shape()
+        s = 128*15*15
+        print ' flatten to ', s
+        flat_1 = tf.reshape(x_, [-1,s])
+    if experiment['network'] == 'fullyconnected_subnetwork_gudi_2015':
+        s = 48*48
+        print ' flatten to ', s
+        flat_1 = tf.reshape(x_, [-1,s])
+    # print h_conv3_flat.get_shape()
 
-print h_conv2.get_shape()
 
-W_conv3 = weight_variable([4, 4, 64, 128])
-b_conv3 = bias_variable([128])
+    W1 = weight_variable([s,3072])
+    b1 = bias_variable([3072])
+    print 'Fully connected matrix 1 : ', W1.get_shape(), 'with bias',b1.get_shape()
 
-h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3,padding="VALID") + b_conv3)
+    full_1 = tf.nn.relu(tf.matmul(flat_1,W1)+b1)
 
-print h_conv3.get_shape()
+    W2 = weight_variable([3072,12])
+    b2 = bias_variable([12])
 
-s = 128*15**2
-flat_1 = tf.reshape(h_conv3, [-1,s])
-# print h_conv3_flat.get_shape()
+    print 'Fully connected matrix 2 : ', W2.get_shape(), 'with bias',b2.get_shape()
 
-W1 = weight_variable([s,500])
-b1 = bias_variable([500])
+    tmp = tf.nn.softmax(tf.matmul(full_1,W2) + b2)
+    y_conv = tf.transpose(tf.mul(tf.reduce_sum(tf.cast(y_,tf.float32),1),tf.transpose(tmp)))
+    # y_conv = tmp
+    #*tf.reduce_sum(y_,1))
 
-full_1 = tf.nn.relu(tf.matmul(flat_1,W1)+b1)
-print full_1.get_shape()
+    print 'Output shape : ', y_conv.get_shape()
 
-W2 = weight_variable([500,12])
-b2 = bias_variable([12])
 
-y_conv = tf.nn.sigmoid(tf.matmul(full_1,W2) + b2)
-THRESHOLD = 0.3
-a = tf.cast(tf.greater(y_conv,THRESHOLD),tf.float32)
-b = tf.cast(tf.greater(y_,THRESHOLD),tf.float32)
-c = tf.abs(a-b)
-ab_sum = tf.reduce_sum(a,1)+tf.reduce_sum(b,1)
-accuracy = tf.reduce_mean(tf.Variable(1.0)-tf.reduce_sum(c,1)/ab_sum)
-print y_conv.get_shape()
-cross_entropy = -tf.reduce_sum(y_*tf.log(tf.clip_by_value(y_conv,1e-10,1.0)))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    THRESHOLD = experiment['threshold']
+    a = tf.cast(tf.greater(y_conv,THRESHOLD),tf.float32)
+    b = tf.cast(tf.greater(y_,THRESHOLD),tf.float32)
+    c = tf.abs(a-b)
+    ab_sum = tf.reduce_sum(a,1)+tf.reduce_sum(b,1)
+    accuracy = tf.reduce_mean(tf.Variable(1.0)-tf.reduce_sum(c,1)/ab_sum)
 
-lmsq_loss = tf.sqrt(tf.reduce_mean(tf.square(y_conv-y_)))
-#train_step = tf.train.GradientDescentOptimizer(0.10).minimize(lmsq_loss)
-#correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+    cross_entropy = -tf.reduce_sum(y_*tf.log(tf.clip_by_value(y_conv,1e-10,1.0)))
+    lmsq_loss = tf.sqrt(tf.reduce_mean(tf.square(y_conv-y_)))
 
-config = tf.ConfigProto()
-config.gpu_options.allow_growth=True
-sess = tf.Session(config=config)
+    if experiment['cost_function'] == 'cross_entropy':
+        train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    else:
+        train_step = tf.train.GradientDescentOptimizer(0.10).minimize(lmsq_loss)
 
-init = tf.initialize_all_variables()
-sess.run(init)
-import decimal
-print all_images.shape
-print targets_all.shape
-# print type(Images[0,0,0])
-o = 1000
-i_validation,i_train = np.array_split(all_images, 2,axis=0)
-l_validation,l_train = np.array_split(targets_all,2,axis=0)
+    if socket.gethostname() == 'ux305':
+        sess = tf.Session()
+    else:
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth=True
+        sess = tf.Session(config=config)
 
-c = int(i_train.shape[0]/1000.0 - 1)
-#c = 100000
-for i in xrange(0,c):
-    L = i*o
-    R = i*o + o
-    batch_x = i_train[L:R,:,:]
-    batch_y = l_train[L:R,:]
-    vbatch_x = i_validation[L:R,:,:]
-    vbatch_y = l_validation[L:R,:]
-    bs = batch_y.shape
-    vs = vbatch_y.shape
-    print bs,vs
-    print L,R,
-    'train step', sess.run(train_step, feed_dict={x: batch_x, y_: batch_y}),
-    print 'lmsq_loss', sess.run(lmsq_loss, feed_dict={x: vbatch_x, y_: vbatch_y}),
-    print 'centropy', sess.run(cross_entropy, feed_dict={x: vbatch_x, y_: vbatch_y}),
-    # print 'correct_prediction', sess.run(correct_prediction, feed_dict={x: batch_x, y_: batch_y})
-    print 'accuracy', sess.run(accuracy, feed_dict={x: vbatch_x, y_: vbatch_y}),
-    out =  sess.run(y_conv, feed_dict={x: batch_x, y_: batch_y})
-    if(i < 10 or i > 1500):
-        np.savetxt('out_'+str(i)+'.txt',out)
-        np.savetxt('gut_'+str(i)+'.txt',batch_y)
-    print 'output range: ', out.min(),out.max(),
-    print
+    init = tf.initialize_all_variables()
+    sess.run(init)
 
-sess.close()
+    def roc_area(y_true, y_score,feed_dict):
+        for i in xrange(y_true.shape[1]):
+            trues = y_true[:,i]
+            scores = sess.run(y_score,feed_dict)[:,i]
+            if trues.sum() != 0.0:
+                # print trues,scores
+                fpr, tpr, t = roc_curve(trues,scores)
+                print fpr, tpr, t
+                roc_auc = auc(fpr, tpr)
+                print roc_auc
+            else:
+                print '?',
+        print
+
+    N = experiment['iterations']
+    random_batch = experiment['batch_randomisation']
+    batch_size = experiment['batch_size']
+    x_axis = np.zeros(N)
+    lmsq_axis = np.zeros((2,N))
+    cent_axis = np.zeros((2,N))
+    accu_axis = np.zeros((2,N))
+    train_auac_axis = np.zeros((N,12,4))
+    test_auac_axis = np.zeros((N,12,4))
+    train_confusion = []
+    test_confusion = []
+    print 'i    test    train    test    train    test    train'
+    print 'i    lmsq    lmsq    cent    cent    accu    accu'
+    ref = sess.run(tf.reduce_sum(data.train.next_batch(-1,False)[1],0))
+    # raw_input('Fuck off')
+    path = experiment.get_path()
+    for i in xrange(N):
+        print i
+        batch_x, batch_y = data.train.next_batch(batch_size,random_batch)
+        t_batch_x, t_batch_y = data.test.next_batch(batch_size,random_batch)
+        train = {x: batch_x, y_: batch_y}
+        test = {x: t_batch_x, y_: t_batch_y}
+        sess.run(train_step, feed_dict={x: batch_x, y_: batch_y})
+        x_axis[i] = i
+        e = [lmsq_loss,cross_entropy,accuracy]
+        lmsq_axis[0,i],cent_axis[0,i],accu_axis[0,i] = sess.run(e, feed_dict={x: t_batch_x, y_: t_batch_y})
+        lmsq_axis[1,i],cent_axis[1,i],accu_axis[1,i] = sess.run(e, feed_dict={x: batch_x, y_: batch_y})
+        # print i,'   \t',
+        # print round(lmsq_axis[0,i],2),' ',
+        # print round(lmsq_axis[1,i],2),' ',
+        # print round(cent_axis[0,i],2),' ',
+        # print round(cent_axis[1,i],2),' ',
+        # print round(accu_axis[0,i],2),' ',
+        # print round(accu_axis[1,i],2),' '
+        y_out = sess.run(y_conv, feed_dict={x: batch_x, y_: batch_y})
+        # print y_out
+        # print batch_y
+        # print ref
+        train_res, train_conf = metric.multi_eval(y_out,batch_y)
+        train_auac_axis[i,:,:] = train_res
+        test_res, test_conf = metric.multi_eval(y_out,t_batch_y)
+        test_auac_axis[i,:,:] = test_res
+
+        train_confusion.append(train_conf)
+        test_confusion.append(test_conf)
+
+        # print res,
+        # print
+    sess.close()
+
+    ssv_path = os.path.join(path,'numerical_data')
+    if not os.path.isdir(ssv_path):
+        os.mkdir(ssv_path)
+    np.savetxt(os.path.join(ssv_path,'x_axis.ssv'),x_axis)
+    np.savetxt(os.path.join(ssv_path,'lmsq.ssv'),lmsq_axis)
+    np.savetxt(os.path.join(ssv_path,'cross_entropy.ssv'),cent_axis)
+    np.savetxt(os.path.join(ssv_path,'naive_accuracy.ssv'),accu_axis)
+    np.savez(os.path.join(ssv_path,'per_au_accuracy.npz'),
+             train_metrics=train_auac_axis,
+             test_metrics=test_auac_axis,
+             train_confusion=train_confusion,
+             test_confusion=test_confusion
+             )
+
+
+experiment =  PyExp(config_file='config/cnn.yaml',path='data')
+if experiment['data']['dataset'] == 'disfa':
+    import disfa
+    data = disfa.Disfa(number_of_subjects=3,train_prop=1,valid_prop=1,test_prop=1)
+    main(data,experiment)
+else:
+    from tensorflow.examples.tutorials.mnist import input_data
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    main(mnist,experiment)
---
+++ src/plotexp.py
@@ -0,0 +1,88 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+plt.style.use('bmh')
+
+path = 'data/2016_05_29/014'
+ssv_path = os.path.join(path,'numerical_data')
+
+x_axis = np.loadtxt(os.path.join(ssv_path,'x_axis.ssv'))
+lmsq_axis = np.loadtxt(os.path.join(ssv_path,'lmsq.ssv'))
+cent_axis = np.loadtxt(os.path.join(ssv_path,'cross_entropy.ssv'))
+accu_axis = np.loadtxt(os.path.join(ssv_path,'naive_accuracy.ssv'))
+per_au = np.load(os.path.join(ssv_path,'per_au_accuracy.npz'))
+test_auac_axis = per_au['test_metrics']
+train_auac_axis = per_au['train_metrics']
+
+plt.figure()
+plt.title('Least mean squared error')
+plt.plot(x_axis,lmsq_axis[0,:],label='test')
+plt.plot(x_axis,lmsq_axis[1,:],label='train')
+plt.ylim(-0.1,1.1)
+plt.legend()
+plt.savefig(os.path.join(path,'lmsq.png'),dpi=400)
+
+plt.figure()
+plt.title('Cross entropy')
+plt.plot(x_axis,cent_axis[0,:],label='test')
+plt.plot(x_axis,cent_axis[1,:],label='train')
+plt.legend()
+plt.ylim(-1.0,cent_axis.max()+1.0)
+plt.savefig(os.path.join(path,'cross_entropy.png'),dpi=400)
+
+plt.figure()
+plt.title('Naive accuracy')
+plt.plot(x_axis,accu_axis[0,:],label='test')
+plt.plot(x_axis,accu_axis[1,:],label='train')
+plt.ylim(-0.1,1.1)
+plt.legend()
+plt.savefig(os.path.join(path,'accuracy.png'),dpi=400)
+
+def au(x_axis,auac_axis,prefix):
+
+    N,classes,calcs = auac_axis.shape
+    for i in xrange(classes):
+        for j in xrange(calcs):
+            for k in xrange(N):
+                if np.isnan(auac_axis[k,i,j]):
+                    auac_axis[k,i,j] = 0.0
+
+
+    nN = N/1000
+    nxaxis = np.zeros(nN)
+    smoothed = np.zeros((nN,classes,calcs))
+
+    for i in xrange(classes):
+        for j in xrange(calcs):
+            for n in xrange(nN):
+                l = n*100
+                r = (n+1)*100
+                if r == N:
+                    nxaxis[n] = x_axis[r-1]
+                else:
+                    nxaxis[n] = x_axis[r]
+                smoothed[n,i,j] = auac_axis[l:r,i,j].mean()
+
+    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex='col', sharey='row')
+    axes = [ax1, ax2,ax3, ax4]
+    titles = ['precision','recall','f1 score','area under roc']
+    for i in xrange(classes):
+        for j in xrange(len(axes)):
+            axes[j].set_title(titles[j])
+            axes[j].plot(nxaxis, smoothed[:,i,j],label=str(i))
+            axes[j].set_ylim(-0.1,1.1)
+    art = []
+    lgd = plt.legend(loc=9, bbox_to_anchor=(-0.1, -0.1), ncol=4)
+    art.append(lgd)
+    plt.ylim(-0.1,1.1)
+    labels = ax4.get_xticklabels()
+    plt.setp(labels, rotation=30, fontsize=10)
+    labels = ax3.get_xticklabels()
+    plt.suptitle(prefix)
+    plt.setp(labels, rotation=30, fontsize=10)
+    plt.savefig(
+        os.path.join(path,prefix+'_per_au.png'),dpi=400, additional_artists=art,
+        bbox_inches="tight")
+
+au(x_axis,train_auac_axis,'train')
+au(x_axis,test_auac_axis,'test')
