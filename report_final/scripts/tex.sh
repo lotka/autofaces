9465b8586eb072b4e097925e7c90e91056d2a3de
Luka Milic <lukam321@gmail.com>
2016-09-02 20:45:56 +0100

    improved implementation section and slowly attacking the model section

--- report_final/scripts/tex.sh
+++ report_final/scripts/tex.sh
@@ -3,6 +3,6 @@ pdflatex -halt-on-error main.tex
 bibtex main
 pdflatex -halt-on-error main.tex
 pdflatex -halt-on-error main.tex
-mv main.pdf report.pdf
+cp main.pdf report.pdf
 #python clean.py
 rm -rf *.aux *.toc *.out *.bbl *.blg *.log *.brf
